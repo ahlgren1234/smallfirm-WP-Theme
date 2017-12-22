139b6c06b66a2c455af4b5fa093cfd6e91756151
Peter Ahlgren <ahlgren1234@gmail.com>
2017-12-22 22:44:06 +0100

    Added the loop

--- index.php
+++ index.php
@@ -16,7 +16,23 @@
             <div class="container">
                 <div class="row">
                     <aside class="sidebar col-md-3">Sidebar</aside>
-                    <div class="news col-md-9">News</div>
+                    <div class="news col-md-9">
+
+                        <?php if( have_posts() ): ?>
+                            <?php while( have_posts() ): the_post() ?>
+                                <article>
+                                    <h2><?php the_title(); ?></h2>
+                                    <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+                                    <p>Categories: <?php the_category( ', ' ); ?></p>
+                                    <p><?php the_tags( 'Tags: ', ', ' ); ?></p>
+                                    <p><?php the_content(); ?></p>
+                                </article>
+                            <?php endwhile; ?>
+                        <?php else: ?>
+                            <p>There's nothing yet to be shown.</p>
+                        <?php endif; ?>
+
+                    </div><!-- .news -->
                 </div>
             </div><!-- .container -->
 		</section><!-- .middle-area -->
