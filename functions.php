bd5fa98cb3dfd601ec9e4e095220c5bc008a10c9
Peter Ahlgren <ahlgren1234@gmail.com>
2017-12-23 15:42:27 +0100

    iohjio

--- functions.php
+++ functions.php
@@ -27,6 +27,7 @@ function smallfirm_config() {
     );
     add_theme_support( 'custom-header', $args );
     add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'post-formats', array( 'video', 'image' ) );
 
 }
 add_action( 'after_setup_theme', 'smallfirm_config', 0 );
